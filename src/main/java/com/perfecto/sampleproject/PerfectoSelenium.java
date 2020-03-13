b178407645d5e8820940ac09d4bb6f727e54ab91
Dushyantan Satike <dushyantans@perfectomobile.com>
2020-03-13 16:14:22 +0530

    First Commit

---
+++ src/main/java/com/perfecto/sampleproject/PerfectoSelenium.java
@@ -0,0 +1,95 @@
+package com.perfecto.sampleproject;
+import com.perfecto.sampleproject.Utils;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.perfecto.reportium.client.ReportiumClient;
+import com.perfecto.reportium.client.ReportiumClientFactory;
+import com.perfecto.reportium.model.Job;
+import com.perfecto.reportium.model.PerfectoExecutionContext;
+import com.perfecto.reportium.model.Project;
+import com.perfecto.reportium.test.TestContext;
+import com.perfecto.reportium.test.result.TestResult;
+import com.perfecto.reportium.test.result.TestResultFactory;
+
+
+public class PerfectoSelenium {
+
+	ReportiumClient reportiumClient = null;
+	RemoteWebDriver driver = null;
+	@Test
+	public void main() throws Exception {
+		//Replace <<cloud name>> with your perfecto cloud name (e.g. demo) or pass it as maven properties: -DcloudName=<<cloud name>>  
+		String cloudName = "<<cloud name>>";
+		//Replace <<security token>> with your perfecto security token or pass it as maven properties: -DsecurityToken=<<SECURITY TOKEN>>  More info: https://developers.perfectomobile.com/display/PD/Generate+security+tokens
+		String securityToken = "<<SECURITY TOKEN>>";
+
+		String browserName = "mobileOS";
+		DesiredCapabilities capabilities = new DesiredCapabilities(browserName, "", Platform.ANY);
+		capabilities.setCapability("securityToken", Utils.fetchSecurityToken(securityToken));
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("useAppiumForWeb", true);
+		capabilities.setCapability("browserName","Safari");
+		driver = new RemoteWebDriver(new URL("https://" + Utils.fetchCloudName(cloudName) + ".perfectomobile.com/nexperience/perfectomobile/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+		// Reporting client. For more details, see https://developers.perfectomobile.com/display/PD/Java
+		PerfectoExecutionContext perfectoExecutionContext;
+		if(System.getProperty("reportium-job-name") != null) {
+			perfectoExecutionContext = new PerfectoExecutionContext.PerfectoExecutionContextBuilder()
+					.withProject(new Project("My Project", "1.0"))
+					.withJob(new Job(System.getProperty("reportium-job-name") , Integer.parseInt(System.getProperty("reportium-job-number"))))
+					.withContextTags("tag1")
+					.withWebDriver(driver)
+					.build();
+		} else {
+			perfectoExecutionContext = new PerfectoExecutionContext.PerfectoExecutionContextBuilder()
+					.withProject(new Project("My Project", "1.0"))
+					.withContextTags("tag1")
+					.withWebDriver(driver)
+					.build();
+		}
+		reportiumClient = new ReportiumClientFactory().createPerfectoReportiumClient(perfectoExecutionContext);
+		System.out.println(System.getProperty("rolloutUrl"));
+		reportiumClient.testStart("Perfecto mobile web test", new TestContext("tag2", "tag3"));
+		reportiumClient.stepStart("browser navigate to perfecto");
+		driver.get(System.getProperty("rolloutUrl"));
+		reportiumClient.stepEnd();
+
+		reportiumClient.stepStart("Verify title");
+		String aTitle = driver.getTitle();
+		//compare the actual title with the expected title
+		if (!aTitle.equals("Web & Mobile App Testing | Continuous Testing | Perfecto"))
+			throw new RuntimeException("Title is mismatched");
+		reportiumClient.stepEnd();
+
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+		//STOP TEST
+		TestResult testResult = null;
+
+		if(result.getStatus() == result.SUCCESS) {
+			testResult = TestResultFactory.createSuccess();
+		}
+		else if (result.getStatus() == result.FAILURE) {
+			testResult = TestResultFactory.createFailure(result.getThrowable());
+		}
+		reportiumClient.testStop(testResult);
+
+		driver.close();
+		driver.quit();
+		// Retrieve the URL to the DigitalZoom Report 
+		String reportURL = reportiumClient.getReportUrl();
+		System.out.println(reportURL);
+	}
+}
+
